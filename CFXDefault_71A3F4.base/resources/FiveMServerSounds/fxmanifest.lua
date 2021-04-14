9437101b47248eef5fea8a46c392f4d593b3ae05
EricNakamura <masaro.eric@gmail.com>
2021-04-14 22:27:19 +0100

    added FiveMServerSounds

---
+++ CFXDefault_71A3F4.base/resources/FiveMServerSounds/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+  "audio/sfx/streamed_vehicles_granular/**",
+  "audio/sfx/streamed_vehicles_granular_npc/**",
+  'audio/sfx/resident/**',
+  'audio/sfx/weapons_player/**',
+}
+
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/resident'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/weapons_player'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/streamed_vehicles_granular'
+data_file 'AUDIO_WAVEPACK' 'audio/sfx/streamed_vehicles_granular_npc'
